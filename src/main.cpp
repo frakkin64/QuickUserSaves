d5f3cbfd078f50e8abc4f74ecc0c591c547914f2
frakkin64 <258024685+frakkin64@users.noreply.github.com>
2026-05-19 16:03:32 -0700

    feat: multi-runtime build

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,79 @@
+namespace Hooks
+{
+	enum class SAVEFILE_CATEGORY : std::int32_t
+	{
+		kUser = 0x0,
+		kAuto = 0x1,
+		kQuick = 0x2,
+		kExit = 0x3
+	};
+
+	class hkQuickLoad
+	{
+	private:
+		static void QueueSaveLoadTask(RE::BGSSaveLoadManager* a_this, RE::BGSSaveLoadManager::QUEUED_TASK a_task)
+		{
+			if (a_task == RE::BGSSaveLoadManager::QUEUED_TASK::kQuickLoad) {
+				return _QueueSaveLoadTask(a_this, RE::BGSSaveLoadManager::QUEUED_TASK::kLoadMostRecentSave);
+			}
+
+			return _QueueSaveLoadTask(a_this, a_task);
+		}
+
+		static inline REL::Relocation<decltype(QueueSaveLoadTask)> _QueueSaveLoadTask;
+
+	public:
+		static void Install()
+		{
+			auto& trampoline = REL::GetTrampoline();
+			_QueueSaveLoadTask = trampoline.write_call<5>(REL::Relocation<uintptr_t>{ REL::ID{ 1470086, 2249427 }, REL::Offset{ 0xC3, 0xC3 } }.address(), & QueueSaveLoadTask);
+		}
+	};
+
+	class hkQuickSave
+	{
+	private:
+		static void GenerateSaveFileName(RE::BGSSaveLoadManager* a_this, char* a_saveFileName, bool a_displayOnly, [[maybe_unused]] SAVEFILE_CATEGORY a_saveCategory)
+		{
+			a_this->quicksaveFileName = RE::BSFixedString("");
+			return _GenerateSaveFileName(a_this, a_saveFileName, a_displayOnly, SAVEFILE_CATEGORY::kUser);
+		}
+
+		static inline REL::Relocation<decltype(GenerateSaveFileName)> _GenerateSaveFileName;
+
+	public:
+		static void Install()
+		{
+			auto& trampoline = REL::GetTrampoline();
+			_GenerateSaveFileName = trampoline.write_call<5>(REL::Relocation<uintptr_t>{ REL::ID{ 1055666, 2228083 }, REL::Offset{ 0x32, 0x1ED } }.address(), & GenerateSaveFileName);
+		}
+	};
+}
+
+F4SE_PLUGIN_QUERY(const F4SE::QueryInterface* a_f4se, F4SE::PluginInfo* a_info)
+{
+	if (const auto data = F4SE::PluginVersionData::GetSingleton()) {
+		a_info->infoVersion = F4SE::PluginInfo::kVersion;
+		a_info->name = data->GetPluginName().data();
+		a_info->version = data->GetPluginVersion().pack();
+	}
+
+	const auto ver = a_f4se->RuntimeVersion();
+	if (ver < REL::Version(F4SE::RUNTIME_1_10_163)) {
+		return false;
+	}
+
+	return true;
+}
+
 F4SE_PLUGIN_LOAD(const F4SE::LoadInterface* a_f4se)
 {
-	F4SE::Init(a_f4se);
+	F4SE::Init(a_f4se, { .trampoline = true, .trampolineSize = 32 });
 
-	REX::INFO("Hello World!");
+	Hooks::hkQuickLoad::Install();
+	Hooks::hkQuickSave::Install();
+
+	REX::INFO("Loaded"sv);
 
 	return true;
 }
--- xmake.lua
+++ xmake.lua
@@ -2,8 +2,8 @@
 includes("lib/commonlibf4")
 
 -- set project constants
-set_project("commonlibf4-template")
-set_version("0.0.0")
+set_project("QuickUserSaves")
+set_version("1.1.0")
 set_license("GPL-3.0")
 set_languages("c++23")
 set_warnings("allextra")
@@ -13,11 +13,11 @@ add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
 -- define targets
-target("commonlibf4-template")
+target("QuickUserSaves")
     add_rules("commonlibf4.plugin", {
-        name = "commonlibf4-template",
-        author = "libxse",
-        description = "F4SE plugin template using CommonLibF4"
+        name = "QuickUserSaves",
+        author = "frakkin64",
+        description = "F4SE plugin using CommonLibF4"
     })
 
     -- add src files
